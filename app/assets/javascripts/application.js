48ab74c0c290a42ce4340da96a8861386a0859e2
Renato Ramiro <renato.ipj@gmail.com>
2012-11-30 21:38:54 -0300

    organize application.js file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require bootstrap
-//= require jquery-ui
 //= require jquery
+//= require jquery-ui
 //= require jquery_ujs
+//= require bootstrap
 //= require_tree .
